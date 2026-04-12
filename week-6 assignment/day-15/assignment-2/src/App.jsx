394bf079668d88d6d9de3c0b9b8250381e93f97e
adithya <24eg110a09@anurag.edu.in>
2026-04-12 15:43:47 +0530

    added

--- week-6 assignment/day-15/assignment-2/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-import Users from './components/Users.jsx'
-function App(){
-  return (
-    <Users/>
-  )
-}
-export default App
--- week-6 assignment/day-15/assignment-2/src/components/UsersCount.jsx
+++
@@ -1,9 +0,0 @@
-function UsersCount({ count }) {
-  return (
-    <h1 className="text-center border-2 mb-6 p-2 rounded-xl text-lg">
-      Users Added: {count}
-    </h1>
-  );
-}
-
-export default UsersCount;
