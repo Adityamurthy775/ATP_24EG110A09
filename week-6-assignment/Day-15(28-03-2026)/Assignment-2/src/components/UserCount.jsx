4f21ad9a90e6ed6bc894abc248c9af6f3d718132
Aditya Murthy <24eg110a09@anurag.edu.in>
2026-04-12 20:09:59 +0530

    Update UserCount.jsx

--- week-6-assignment/Day-15(28-03-2026)/Assignment-2/src/components/UserCount.jsx
+++ week-6-assignment/Day-15(28-03-2026)/Assignment-2/src/components/UserCount.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function UserCount(props) {
   return (
     <div className="flex justify-center mb-6">
-      <div className="w-64 bg-gray-100 rounded-xl shadow-md p-5 text-center">
+      <div className="w-64 bg-gray-500 rounded-xl shadow-md p-5 text-center">
         <h6 className="text-gray-500 mb-2">Users Added</h6>
 
         <h2 className="text-3xl font-bold text-gray-800">{props.count}</h2>
